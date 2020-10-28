58f03d249f9179157419d248bed72e4ae23632cf
Vincent Morello <vmorello@gmail.com>
2020-10-28 18:24:24 +0000

    Added implementation and some tests, to be improved

--- iqrm/core.py
+++ iqrm/core.py
@@ -1,16 +1,54 @@
+import itertools
 import numpy as np
 
 
-def get_mask(s, maxlag=3, nsigma=3.0):
+def shdiff(x, lag):
+    """
+    Returns the sequence of x[i] - x[i - lag], as an array with the same size as x.
+    Boundary conditions are handled as follows:
+        x[i] = x[0]   for i < 0
+        x[i] = x[n-1] for i >= n, where n = len(x)
+    """
+    s = np.roll(x, lag)
+
+    if lag >= 0:
+        s[:lag] = x[0]
+    else:
+        # NOTE: lag is NEGATIVE here
+        s[lag:] = x[-1]
+    return x - s
+
+
+def outlier_mask(x, nsigma):
+    """
+    Returns an outlier mask for array x. TODO: describe the method.
+    """
+    q1, med, q3 = np.percentile(x, [25, 50, 75])
+    std = (q3 - q1) / 1.349
+    return (x - med) > nsigma * std
+
+
+def get_mask(s, maxlag=5, nsigma=3.0):
     """
     TODO
 
     Parameters
     ----------
     s : list or ndarray
+        Bandpass statistic, e.g. per-channel standard deviation, from which to infer a channel mask
     maxlag : int
+        Maximum shdiff lag considered for detecting outliers
     nsigma : float
+        Flag as an outlier any channel for which any shdiff is further from expectation than 
+        'nsigma' standard deviations.
+
+    Returns
+    -------
+    mask : ndarray
+        Boolean mask with the same size as the input data, where 'True' represents a bad channel
     """
+    s = np.asarray(s)
+
     if not (isinstance(maxlag, int) and maxlag > 0):
         raise ValueError("maxlag must be an int > 0")
 
@@ -18,4 +56,10 @@ def get_mask(s, maxlag=3, nsigma=3.0):
     if not nsigma > 0:
         raise ValueError("nsigma must be > 0")
 
-    return np.ones_like(s)
+    mask = np.zeros_like(s, dtype=bool)
+
+    # Go through all lags in [-maxlag, +maxlag], except 0
+    for lag in itertools.chain(range(-maxlag, 0), range(1, maxlag+1)):
+        d = shdiff(s, lag)
+        mask = mask | outlier_mask(d, nsigma)
+    return mask
--- iqrm/tests/test_iqrm.py
+++ iqrm/tests/test_iqrm.py
@@ -4,16 +4,49 @@ from pytest import raises
 import iqrm
 
 
-def generate_data(nchan=1024, seed=0):
+def generate_noise(nchan=1024, seed=0):
+    # IMPORTANT: set a random seed for reproducible results
     np.random.seed(seed)
     return np.random.normal(size=nchan)
+
+
+def generate_noise_with_outlier_range(start, end, nchan=1024, seed=0):
+    s = generate_noise(nchan=nchan, seed=seed)
+    s[start:end] += 100
+    return s
 
 
 def test_param_checks():
     pass
 
 
-def test_masking():
-    s = generate_data()
-    mask = iqrm.get_mask(s)
-    assert np.allclose(mask, np.ones_like(s))
+def test_masking_noise():
+    s = generate_noise()
+
+    for maxlag in range(1, 6):
+        mask = iqrm.get_mask(s, maxlag=maxlag, nsigma=4.0)
+        assert np.alltrue(~mask)
+
+
+def test_masking_single_outlier():
+    nchan = 1024
+    indices = [0, 1, 42, 213, 740, 1022, 1023]
+
+    for index in indices:
+        for maxlag in range(1, 6):
+            s = generate_noise_with_outlier_range(index, index+1, nchan=nchan)
+            mask = iqrm.get_mask(s, maxlag=maxlag, nsigma=4.0)
+            assert mask[index] == True
+
+
+def test_masking_outlier_range():
+    nchan = 1024
+    indices = [0, 1, 42, 213, 740, 1022, 1023]
+
+    for index in indices:
+        for span in range(2, 6):
+            s = generate_noise_with_outlier_range(index, index+span, nchan=nchan)
+            # The whole range of outliers should be masked as long as we use
+            # maxlag > span
+            mask = iqrm.get_mask(s, maxlag=span, nsigma=4.0)
+            assert np.alltrue(mask[index:index+span])
