ebd528110556c2c4d15f270a1caae78ea91e5f4f
Ujjwal Panda <ujjwalpanda97@gmail.com>
2025-04-09 02:25:52 +0530

    Shift from `versioneer` to `setuptools-scm`.

--- iqrm/__init__.py
+++
@@ -1,6 +0,0 @@
-from ._version import get_versions
-__version__ = get_versions()['version']
-del get_versions
-
-from .core import iqrm_mask
-from .tests import test
---
+++ src/iqrm/__init__.py
@@ -0,0 +1,1 @@
+from .core import iqrm_mask
